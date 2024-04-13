69975ed882d346506d0dfed161d8abcdfbc1a7be
mateus-hba <mateushba@gmail.com>
2024-04-13 05:26:35 -0300

    Script básico de ataque com animações

--- character/scripts/character.gd
+++ character/scripts/character.gd
@@ -1,6 +1,7 @@
 extends CharacterBody2D
 
 var _state_machine
+var _is_attacking: bool = false
 
 @export_category("Variables")
 @export var _move_speed: float = 64.0
@@ -8,6 +9,7 @@ var _state_machine
 @export var _acceleration: float = 0.4
 
 @export_category("Objects")
+@export var _attack_timer: Timer = null
 @export var _animation_tree: AnimationTree = null
 
 func _ready() -> void:
@@ -15,6 +17,7 @@ func _ready() -> void:
 
 func _physics_process(_delta: float) -> void:
 	_move()	
+	_attack()
 	_animate()
 	move_and_slide()
 	
@@ -28,6 +31,8 @@ func _move() -> void:
 	if _direction != Vector2.ZERO:
 		_animation_tree["parameters/idle/blend_position"] = _direction
 		_animation_tree["parameters/walk/blend_position"] = _direction
+		_animation_tree["parameters/attack/blend_position"] = _direction
+		
 		velocity.x = lerp(velocity.x, _direction.normalized().x * _move_speed, _acceleration)
 		velocity.y = lerp(velocity.y, _direction.normalized().y * _move_speed, _acceleration)
 		return
@@ -35,9 +40,25 @@ func _move() -> void:
 	velocity.x = lerp(velocity.x, _direction.normalized().x * _move_speed, _friction)
 	velocity.y = lerp(velocity.y, _direction.normalized().y * _move_speed, _friction)
 		
+func _attack() -> void:
+	if Input.is_action_just_pressed("attack") and not _is_attacking:
+		set_physics_process(false)
+		_attack_timer.start()
+		_is_attacking = true
+	
+		
 func _animate() -> void:
-	if velocity.length() > 3:
+	if _is_attacking:
+		_state_machine.travel("attack")
+		return
+		
+	if velocity.length() > 10:
 		_state_machine.travel("walk")
 		return
 		
 	_state_machine.travel("idle")
+
+
+func _on_attack_timer_timeout() -> void:
+	set_physics_process(true)
+	_is_attacking = false
