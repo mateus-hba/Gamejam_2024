ba1e1d617e4825b1abbf21eaaeb48fd97322d60a
mateus-hba <mateushba@gmail.com>
2024-04-13 04:57:29 -0300

    Criando animações de movimento e idle

--- character/scripts/character.gd
+++ character/scripts/character.gd
@@ -1,12 +1,21 @@
 extends CharacterBody2D
 
+var _state_machine
+
 @export_category("Variables")
 @export var _move_speed: float = 64.0
 @export var _friction: float = 0.8
 @export var _acceleration: float = 0.4
 
+@export_category("Objects")
+@export var _animation_tree: AnimationTree = null
+
+func _ready() -> void:
+	_state_machine = _animation_tree["parameters/playback"]
+
 func _physics_process(_delta: float) -> void:
 	_move()	
+	_animate()
 	move_and_slide()
 	
 func _move() -> void:
@@ -15,8 +24,10 @@ func _move() -> void:
 		Input.get_axis("move_up", "move_down")
 	)
 	
-	#se o usuário não estiver apertando nenhum direcional
+	#se o usuário estiver apertando o direcional
 	if _direction != Vector2.ZERO:
+		_animation_tree["parameters/idle/blend_position"] = _direction
+		_animation_tree["parameters/walk/blend_position"] = _direction
 		velocity.x = lerp(velocity.x, _direction.normalized().x * _move_speed, _acceleration)
 		velocity.y = lerp(velocity.y, _direction.normalized().y * _move_speed, _acceleration)
 		return
@@ -24,4 +35,9 @@ func _move() -> void:
 	velocity.x = lerp(velocity.x, _direction.normalized().x * _move_speed, _friction)
 	velocity.y = lerp(velocity.y, _direction.normalized().y * _move_speed, _friction)
 		
-
+func _animate() -> void:
+	if velocity.length() > 3:
+		_state_machine.travel("walk")
+		return
+		
+	_state_machine.travel("idle")
